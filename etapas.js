353bb4d3ce6d5835be2754973e328bf7b6e746ed
Renan Marques <henrenan@gmail.com>
2021-07-18 23:30:07 -0300

    inicio do Js

---
+++ etapas.js
@@ -0,0 +1,49 @@
+let etapas = [
+    {
+    titulo: 'VEREADOR',
+    numeros: 5,
+    candidatos: [
+            {
+            numero: 38111,
+            nome: 'Fulano de Tal',
+            partido:
+            fotos:[
+                { url:'./imagens/renan.jpg', legenda: 'Vereador'}
+            ] 
+        },
+        {
+            numero: 77222,
+            nome: 'Ciclano da Silva',
+            partido:
+            fotos:[{ url:'./imagens/hadassa.jpg', legenda: 'Vereador'}
+            ] 
+            },
+    ]
+        },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 99,
+                nome: 'Marrom',
+                partido: 'ABC',
+                vice: 'Azul',
+                fotos:[
+                    {url:'./imagens/renan.jpg', legenda: 'Prefeito'},
+                    {url:'./imagens/renan.jpg', legenda: 'Vice-Prefeito', small:true}
+                ]
+            },
+            {
+                numero: 84,
+                nome: 'Vermelho',
+                partido: 'CDE',
+                vice: 'Verde',
+                fotos:[
+                    {url:'./imagens/renan.jpg', legenda: 'Prefeito'},
+                    {url:'./imagens/renan.jpg', legenda: 'Vice-Prefeito', small:true}
+                ]
+            },
+    ]
+    }
+];
